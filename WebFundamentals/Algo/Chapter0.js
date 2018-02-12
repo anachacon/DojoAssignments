9e41fdf117b6a04ebf20019901e7ed95099f4aa0
Ana Chacon <isabel@cogwheelclinic.com>
2018-02-12 15:55:46 -0800

    odd Integers v2

--- WebFundamentals/Algo/Chapter0.js
+++ WebFundamentals/Algo/Chapter0.js
@@ -145,10 +145,8 @@ function whatdoYouKnow(incoming){
 function oddintegers(){
 	"use strict";
 	var sum = 0;
-	for (var i=-300000; i<=300000; i++){
-		if(i%2 !== 0){
+	for (var i=-299999; i<300000; i+=2){
 			sum += i;
-		}
 	}
 	console.log(sum);
 }
