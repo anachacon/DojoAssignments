6fc858198fdfb482e0977110c42583fec4fa8626
Ana Chacon <isabel@cogwheelclinic.com>
2018-02-12 15:52:57 -0800

    odd integers

--- WebFundamentals/Algo/Chapter0.js
+++ WebFundamentals/Algo/Chapter0.js
@@ -140,3 +140,15 @@ function whatdoYouKnow(incoming){
 	console.log(incoming);
 }
 
+//Add odd integers from -300,000 to 300,000, and console.log the final sum. Is there a shortcut?
+
+function oddintegers(){
+	"use strict";
+	var sum = 0;
+	for (var i=-300000; i<=300000; i++){
+		if(i%2 !== 0){
+			sum += i;
+		}
+	}
+	console.log(sum);
+}
