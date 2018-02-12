3cfeb0360b4f3bab4678cc1cd0cb72e40b388cd3
Ana Chacon <isabel@cogwheelclinic.com>
2018-02-12 14:53:09 -0800

    Leap year

--- WebFundamentals/Algo/Chapter0.js
+++ WebFundamentals/Algo/Chapter0.js
@@ -68,3 +68,25 @@ function yourBirthday(x,y){
 		console.log("Just another day...");
 	}
 }
+
+//Write a function that determines whether a given year is a leap year. If a year is divisible by four, it is a leap year, unless it is divisible by 100. However, if it is divisible by 400, then it is.
+
+function leapYear(year){
+	"use strict";
+	if( year%4 === 0 ){
+		if( year%100 === 0 ){
+			if( year%400 === 0 ){
+				console.log("Leap year");
+			}
+			else{
+				console.log("Not a leap year");
+			}
+		}
+		else {
+			console.log("Leap year");
+		}
+	}
+	else{
+		console.log("Not a leap year");
+	}
+}
