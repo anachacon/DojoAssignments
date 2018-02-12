98d2a65e55e271aaac4b6c3acaa20d862789e1ae
Ana Chacon <isabel@cogwheelclinic.com>
2018-02-12 15:35:58 -0800

    Coding Dojo

--- WebFundamentals/Algo/Chapter0.js
+++ WebFundamentals/Algo/Chapter0.js
@@ -114,3 +114,24 @@ function multiplesofSix(){
 	}
 }
 
+//Print integers 1 to 100. If divisible by 5, print "Coding" instead. If by 10, also print " Dojo".
+
+function CodingDojo(){
+	"use strict";
+	for(var i=1; i<=100; ++i){
+		if(i%5 === 0){
+			if (i%10 === 0){
+				console.log("Coding Dojo");
+			}
+			else{
+				console.log("Coding");
+			}
+		}
+		else{
+			console.log(i);
+		}
+	}
+}
+
+
+
