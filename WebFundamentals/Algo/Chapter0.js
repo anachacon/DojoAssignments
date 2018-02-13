8cc08af6921a902c59d55a9248e90905b80f3a19
Ana Chacon <isabel@cogwheelclinic.com>
2018-02-12 16:41:28 -0800

    Final countdown

--- WebFundamentals/Algo/Chapter0.js
+++ WebFundamentals/Algo/Chapter0.js
@@ -172,3 +172,19 @@ function flexibleCountdown(lowNum, highNum, mult){
 		}
 	}
 }
+
+//Given 4 parameters (param1,param2,param3,param4), print the multiples of param1, starting at param2 and extending to param3. One exception: if a multiple is equal to param4, then skip (don’t print) that one. Do this using a WHILE loop.
+
+function finalCountdown(param1, param2, param3, param4){
+	"use strict";
+	
+	var i = param2;
+	while( i <= param3){
+		if (i%param1===0){
+				if(i !== param4){
+					console.log(i);
+				}
+		}
+		++i;
+	}
+}
