c8b29cd574eac4dc84cf5e5f6d0160d42995035e
isabelchacon <isabel@cogwheelclinic.com>
2018-02-12 18:41:03 -0800

    Whitespaces

--- WebFundamentals/Algo/Chapter0.js
+++ WebFundamentals/Algo/Chapter0.js
@@ -196,9 +196,9 @@ function finalCountdown(param1, param2, param3, param4){
 	var i = param2;
 	while( i <= param3){
 		if (i%param1===0){
-				if(i !== param4){
-					console.log(i);
-				}
+			if(i !== param4){
+				console.log(i);
+			}
 		}
 		++i;
 	}
