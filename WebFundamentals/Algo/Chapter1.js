e94bd3edca3f668d84bad46ad267f67bbd1088a2
Ana Chacon <isabel@cogwheelclinic.com>
2018-02-17 01:37:55 -0800

    Generalized

--- WebFundamentals/Algo/Chapter1.js
+++ WebFundamentals/Algo/Chapter1.js
@@ -54,3 +54,31 @@ function greaterThanSecond(){
 	return(count);
 }
 
+//Write a function that accepts any array, and returns a new array with the array values that are greater than its 2nd value. Print how many values this is. What will you do if the array is only one element long?
+
+function greaterThanGeneralized(arr){
+	"use strict";
+	
+	var count = 0;
+	var newarr = [];
+	
+	if (arr.length === 1){
+		return ("Only one value: " + arr[0]);
+	}
+	else{
+		for (var i=0; i<arr.length; i++){
+			if( arr[i] > arr[1]){
+				count++;
+				newarr.push(arr[i]);
+			}	
+			
+		}
+	console.log(count);
+	return(newarr);
+	}	
+}
+
+
+
+
+
