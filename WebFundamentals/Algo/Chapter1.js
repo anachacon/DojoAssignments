695801e04c8bee9153ba519a8a2c0c8ed465068c
Ana Chacon <isabel@cogwheelclinic.com>
2018-02-17 01:54:09 -0800

    F to Celsius

--- WebFundamentals/Algo/Chapter1.js
+++ WebFundamentals/Algo/Chapter1.js
@@ -114,4 +114,15 @@ function fitTheFirst(arr){
 	
 }
 
+//Kelvin wants to convert between temperature scales. Create fahrenheitToCelsius(fDegrees) that accepts a number of degrees in Fahrenheit, and returns the equivalent temperature as expressed in Celsius degrees. 
+
+//For review, Fahrenheit = (9/5 * Celsius) + 32.
+
+
+function fahrenheitToCelsius(fDegrees){
+	"use strict";
+	
+	return (fDegrees-32) * 5/9;
+}
+
 
