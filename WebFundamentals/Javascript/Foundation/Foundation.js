096ba22c6d3f655409d5b5ce2655783cae466c06
Ana Chacon <isabel@cogwheelclinic.com>
2018-02-18 16:20:36 -0800

    do declare

---
+++ WebFundamentals/Javascript/Foundation/Foundation.js
@@ -0,0 +1,28 @@
+/*Declare variables of the following types:
+4 Numbers
+3 Strings
+2 Booleans
+1 Undefined Variable
+Give the variables descriptive names (ex: string1 and string2)
+Print each one to the console with a description (ex: console.log("The First String:", string1);)
+Try to be creative! (don't use 1, 2, 3, and 4 for your numbers -- test out negatives or decimals!)*/
+
+function DoDeclare(){
+	"use strict";
+	var day= 18;
+	var temp= -40;
+	var weekday= 7;
+	var month= 2;
+	
+	var channel="Weather report";
+	var name="Isabel Chacon";
+	var location="Seattle";
+	
+	var snowing= false;
+	var raining = true;
+	
+	var sunny;
+	
+	console.log("Hello, my name is: "+name+". This is the "+channel+" for "+location+". This is the "+month+" month of the year, "+day+" day of the month and "+weekday+" day of the week. The temperature will be "+temp+" degrees. Changes of snowing are "+snowing+" and of rain are "+raining+". Sun conditions are "+sunny);
+}
+
