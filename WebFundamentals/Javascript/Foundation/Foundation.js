ea7ea7fbe22ab22dbcfd95093e5d080b2a5abf6d
Ana Chacon <isabel@cogwheelclinic.com>
2018-02-18 16:51:42 -0800

    Happy BDAY

--- WebFundamentals/Javascript/Foundation/Foundation.js
+++ WebFundamentals/Javascript/Foundation/Foundation.js
@@ -56,3 +56,33 @@ function tellTime(hour, minute, period){
 	console.log(message);
 }
 
+
+//Days until my bday
+
+function birthday(days){
+	"use strict";
+	
+	for(var i=days; i>=0; --i){
+		if (i >= 30){
+		console.log(i+" days until my birthday. Such a long time... =(");
+		}
+		else if(i <30 && i > 5){
+			console.log(i+" days until my birthday. We're getting closer =)!");
+		}
+		else if(i>0){
+			console.log(i+" DAYS UNTIL MY BIRTHDAY!!!");
+		}
+		else{
+			console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*");
+			console.log("♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪");
+			console.log("*•♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•«");
+		}
+	}
+	
+}
+
+
+
+
+
+
