d480067784f6f7768e19a36f39d77cfcf464573b
isabelchacon <isabel@cogwheelclinic.com>
2018-03-04 21:59:17 -0800

    Pokemon

---
+++ WebFundamentals/jQuery/pokemon/JS/script.js
@@ -0,0 +1,12 @@
+//Javascript
+
+$('document').ready(function(){
+	"use strict";
+	
+	for (var i=1; i<= 151; i++){
+		
+		$('body').append('<img src="http://pokeapi.co/media/sprites/pokemon/'+i+'.png">');
+	
+	}
+	
+});
