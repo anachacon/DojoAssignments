6c55e7da9bbaa4faa8a151c90807133943864bc1
isabelchacon <isabel@cogwheelclinic.com>
2018-03-08 21:57:16 -0800

    Names

---
+++ Python/PythonFundamentals/names.py
@@ -0,0 +1,27 @@
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+for element in students:
+     print (element["first_name"] + " " + element["last_name"])
+
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+for key, value in users.items():
+     print (key)
+     for counter, data in enumerate(value, 1):
+         print (str(counter) +" - "+ data["first_name"]+" "+data["last_name"]+" - "+str(len(data["first_name"])+len(data["last_name"])) ) 
