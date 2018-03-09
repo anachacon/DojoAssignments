feb95cf5323ea30f2d970976be95c324c0b6f3df
isabelchacon <isabel@cogwheelclinic.com>
2018-03-08 22:34:31 -0800

    Lists to Dict

---
+++ Python/PythonFundamentals/liststodic.py
@@ -0,0 +1,7 @@
+
+name = ["Anna", "Eli", "Pariece", "Brendan", "Amy", "Shane", "Oscar"]
+favorite_animal = ["horse", "cat", "spider", "giraffe", "ticks", "dolphins", "llamas", "another", "another"]
+
+newDict = zip(name,favorite_animal)
+newDict = dict(newDict)
+print (newDict)
