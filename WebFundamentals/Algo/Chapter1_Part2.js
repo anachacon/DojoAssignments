70c31527d7f0d38e25cf2b5ce5d0628b0415a01c
Ana Chacon <isabel@cogwheelclinic.com>
2018-03-10 12:43:56 -0800

    Print Low, Return High

--- WebFundamentals/Algo/Chapter1_Part2.js
+++ WebFundamentals/Algo/Chapter1_Part2.js
@@ -12,5 +12,23 @@ arr = [1,-3,-5,0,3,4,-6];
 biggieSize(arr);
 //console.log(arr);
 
+//Create a function that takes array of numbers. The function should print the lowest 
+//value in the array, and return the highest value in the array.
 
+function printandreturn(arr){
+    var low=0;
+    var high=0;
+    for (var i=0; i<arr.length; i++){
+        if (arr[i] > high){
+            high = arr[i];
+        }
+        else if (arr[i] < low){
+            low = arr[i];
+        }
+    }
+    console.log(low);
+    return high;     
+}
 
+arr = [1,-3,-5,0,3,4,-6];
+console.log(printandreturn(arr));
