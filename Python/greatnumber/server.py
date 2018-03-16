db6cf0e0bd0cbca1c8df53d79fe1807a4b8e7910
isabelchacon <isabel@cogwheelclinic.com>
2018-03-15 23:41:16 -0700

    great number v2

---
+++ Python/greatnumber/server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, session
+import random
+app = Flask(__name__)
+app.secret_key = 'ThisIsSecret'
+
+@app.route("/")
+def index():
+    if not session.get('number'):
+        session['number'] = random.randrange(0, 101) 
+    return render_template("index.html")
+
+@app.route('/process', methods=["POST"])
+def process():
+    guess = int(request.form["guess"])
+    number = session.get('number')
+    if guess > number:
+        result = "Too high!"
+    elif guess < number:
+        result = "Too low!"
+    else:
+        result = "win"
+        session.pop('number')
+    return render_template("index.html", result = result, number=number)
+app.run(debug=True)
