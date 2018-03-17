7b2267a44e4a4b459bb427f76c9bc4328114f302
isabelchacon <isabel@cogwheelclinic.com>
2018-03-17 15:11:15 -0700

    Ninja Gold

--- Python/ninjagold/server.py
+++ Python/ninjagold/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, session
-from datetime import datetime
+import datetime
 import random
 app = Flask(__name__)
 app.secret_key = 'ThisIsSecret'
@@ -15,19 +15,26 @@ def index():
 @app.route("/process_money", methods=["POST"])
 def process():
     activity = session.get('activity')
+    time = datetime.datetime.utcnow().replace(microsecond=0)
+
     if request.form["building"] == "farm":
         winnings = random.randrange(10, 20)
         session['gold'] = session.get('gold') + winnings
-        activity.append("You earned "+str(winnings)+" golds from the farm!")
+        activity.append("You earned "+str(winnings)+" golds from the farm! ("+str(time)+")")
+
     if request.form["building"] == "cave":
         winnings = random.randrange(5, 10)
         session['gold'] = session.get('gold') + winnings
         activity.append("You earned "+str(winnings)+" golds from the cave!")
+
     if request.form["building"] == "house":
         winnings = random.randrange(2, 5)
         session['gold'] = session.get('gold') + winnings
         activity.append("You earned "+str(winnings)+" golds from the house!")
+
     if request.form["building"] == "casino":
          print("casino")
+
     return redirect('/')
+
 app.run(debug=True)
