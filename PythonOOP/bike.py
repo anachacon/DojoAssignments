3d7e1e102448be1168cb35cc5da599719d32a604
Ana Chacon <isabel@cogwheelclinic.com>
2018-04-03 15:12:13 -0700

    finished bike

--- PythonOOP/bike.py
+++ PythonOOP/bike.py
@@ -1,8 +1,8 @@
 class Bike(object):
-    def __init__(self, price, max_speed, miles):
+    def __init__(self, price, max_speed):
         self.price = price
         self.max_speed = max_speed
-        self.miles = miles
+        self.miles = 0
     def displayinfo(self):
         print "This bike costs {} dlls. Its maximum speed is {} miles. Total miles: {}.".format(self.price, self.max_speed, self.miles)
         return self
@@ -10,8 +10,15 @@ class Bike(object):
         self.miles = self.miles + 10
         return self
     def reverse(self):
-        self.miles = self.miles - 5
+        if self.miles >= 5:
+            self.miles = self.miles - 5
         return self
-myBike = Bike(150,40,15)
-myBike.ride().displayinfo()
-myBike.reverse().displayinfo()
+
+myBike = Bike(150,40)
+myBike.ride().ride().ride().reverse().displayinfo()
+
+myBike2 = Bike(120,30)
+myBike2 = myBike.ride().ride().reverse().reverse().displayinfo()
+
+myBike3 = Bike(110,60)
+myBike3 = myBike.reverse().reverse().reverse().displayinfo()
