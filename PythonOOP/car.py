b205fef010d27bb860bcf7bb6458fa583839efc7
Ana Chacon <isabel@cogwheelclinic.com>
2018-04-03 15:42:56 -0700

    Finished car

--- PythonOOP/car.py
+++ PythonOOP/car.py
@@ -8,13 +8,13 @@ class Car(object):
             self.tax = price * .15
         else:
              self.tax = price * .12
-        self.displayall()
+        print(self.displayall())
     def displayall(self):
-        print "Price: {} dlls".format(self.price)
-        print "Speed: {} mph".format(self.speed)
-        print "Fuel: {}".format(self.fuel)
-        print "Mileage: {}".format(self.mileage)
-        print "Tax: {}".format(self.tax)
-        return ("The car price is: {}. The speed is: {}. The fuel is: ")
+        return ("The car price is: {}. The speed is: {}. The fuel is: {}. The mileage is: {}. The tax is: {}.".format(self.price, self.speed, self.fuel, self.mileage, self.tax))
 
-myCar = Car (100,100,100,100)
+myCar = Car(10500,160,"Full",100)
+myCar2 = Car(10500,160,"Empty",90)
+myCar3 = Car(10500,160,"Almost Full",80)
+myCar4 = Car(10500,160,"Full",70)
+myCar5 = Car(10500,160,"Full",60)
+myCar6 = Car(10500,160,"Full",50)
