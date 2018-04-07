31f133e0bf7b44dad65ee129c2bfcdbe638c1e8e
Ana Chacon <isabel@cogwheelclinic.com>
2018-04-07 13:13:42 -0700

    Ajax calls file

--- Algos/Ajax/ajaxcalls.js
+++ Algos/Ajax/ajaxcalls.js
@@ -1,34 +1,34 @@
 var datos = "memberid="+memberid+"&firstinitials="+iname+"&lastinitials="+lname+"&birthdate="+dob+"&gender="+gender+"&diagnosis="+diagnosis+"&diagcode="+diagcode;
 
-  $.ajax
-    ({
-        type: 'POST',
-        data: datos,
-        url: 'URL/AddPatient.php',
-        timeout: 2000,
-        async: true,
-        success: function(result)
-                 {			
-                     
-                    var status = result.status;
-                    var patientid = result.patientid;
-                    
-                    if (status === 0){
-                        console.log("Unable to add patient.");  
-                    }
-                     
-                    if (status === 1){
+$.ajax
+({
+    type: 'POST',
+    data: datos,
+    url: 'URL/AddPatient.php',
+    timeout: 2000,
+    async: true,
+    success: function(result){	
+        		
+                var status = result.status;
+                var patientid = result.patientid;
+                
+                switch(status) {
+                    case 0:
+                        console.log("Unable to add patient."); 
+                        break;
+                    case 1:
                         console.log("Patient added.")
                         sessionStorage.setItem('patientid', patientid);
-                    }
-                     
-                    if (status === 2){
+                        break;
+                    case 2:
                         console.log("Patient already exists");
+                        break;
+                    default:
+                        console.log("Unable to add patient.");
                     }
-                     
-                 },
-         error: function()
-                {
-                      alert("error");
-                }
-    }); 
+                },
+        error: function()
+            {
+                    alert("error");
+            }
+}); 
