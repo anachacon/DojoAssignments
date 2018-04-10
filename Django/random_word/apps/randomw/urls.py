7eb3f650592c01a01cc70dd620a3512e525b8d4b
isabelchacon <isabel@cogwheelclinic.com>
2018-04-09 22:52:21 -0700

    Random Word

--- Django/random_word/apps/randomw/urls.py
+++ Django/random_word/apps/randomw/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views           # This line is new!
 urlpatterns = [
-    url(r'^$', views.index)     # This line has changed!
+    url(r'^$', views.index),    # This line has changed!
+    url(r'^random_word$', views.index),
+    url(r'^random_word/reset$', views.reset)
 ]
--- Django/random_word/apps/randomw/views.py
+++ Django/random_word/apps/randomw/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.utils.crypto import get_random_string
 # the index function is called when root is visited
 def index(request):
     response = "Hello, I am your first request!"
@@ -6,4 +7,12 @@ def index(request):
         request.session['counter'] += 1
     else:
         request.session['counter'] = 1
-    return render (request, "randomw/index.html")
+    unique_id = get_random_string(length=14)
+    context = {
+        "word":unique_id
+    }
+    return render (request, "randomw/index.html", context)
+
+def reset(request):
+    del request.session['counter']
+    return redirect ("/")
