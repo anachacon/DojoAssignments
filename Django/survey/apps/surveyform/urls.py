898aebaac10b1bd0659e71692abed029ce40ae24
isabelchacon <isabel@cogwheelclinic.com>
2018-04-10 23:52:53 -0700

    Survey

--- Django/survey/apps/surveyform/urls.py
+++ Django/survey/apps/surveyform/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
-from . import views           # This line is new!
+from . import views         
 urlpatterns = [
-    url(r'^$', views.index)     # This line has changed!
+    url(r'^$', views.index),
+    url(r'^surveys/process$', views.process),
+    url(r'^result$', views.result), 
+    url(r'^reset$', views.reset)   
 ]
--- Django/survey/apps/surveyform/views.py
+++ Django/survey/apps/surveyform/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render, HttpResponse, redirect
   # the index function is called when root is visited
 def index(request):
-    response = "Hello, I am your first request!"
-    return HttpResponse(response)
+  return render(request, "surveyform/index.html")
 
+def process(request):
+  if(not 'counter' in request.session):
+    request.session['counter']=1
+  else:
+    request.session['counter'] = request.session['counter'] + 1
+  request.session['name'] = request.POST['name']
+  request.session['location'] = request.POST['location']
+  request.session['language'] = request.POST['language']
+  request.session['comment'] = request.POST['comment']
+  return redirect ("/result")
+
+
+def result(request):
+  if len(request.session['comment']) > 0:
+    comment = request.session['comment']
+  else:
+    comment = "No comment"
+  context = {
+    "counter" : request.session['counter'],
+    "name" : request.session['name'],
+    "location" : request.session['location'],
+    "language" : request.session['language'],
+    "comment" : comment
+  }
+  return render (request, "surveyform/result.html", context)
+    
+def reset(request):
+  for key in request.session.keys():
+    del request.session[key]
+  return redirect ("/")
