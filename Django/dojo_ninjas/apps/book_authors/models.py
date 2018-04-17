b79c4fd33e8e2a93dde5c1dd8f03a9afd289ad70
isabelchacon <isabel@cogwheelclinic.com>
2018-04-16 22:36:24 -0700

    Books/authors

---
+++ Django/dojo_ninjas/apps/book_authors/models.py
@@ -0,0 +1,33 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    desc = models.TextField(max_length=1000)
+    notes = models.TextField(max_length=1000, default="")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def authors(self):
+        return Book.objects.get(id=this.id).author_set.all()
+     
+
+class Author(models.Model):
+    books_published = models.ManyToManyField(Book, related_name="authors")
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{},{}".format(self.last_name, self.first_name)
+
+    def books(self):
+        return self.books_published.all()
