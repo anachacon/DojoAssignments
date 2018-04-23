c20105fea9ddc383224cb06a360c59e3886a1cb1
isabelchacon <isabel@cogwheelclinic.com>
2018-04-22 19:03:15 -0700

    Belt reviewer - logged in

--- Django/beltreviewer/apps/beltreview/models.py
+++ Django/beltreviewer/apps/beltreview/models.py
@@ -4,3 +4,42 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class UserManager(models.Manager): #default function .get/.filter/.all/.first
+    def validate_data(self, request_data):
+        valid = True
+        errors = []
+        if request_data['name'] == "":
+            valid = False
+            errors.append('Name cannot be empty')
+        if request_data['alias'] == "":
+            valid = False
+            errors.append('Alias cannot be empty')
+        if request_data['email'] == "":
+            valid = False
+            errors.append('Email cannot be empty')
+        if request_data['password'] == "":
+            valid = False
+            errors.append('Password cannot be empty')
+        if len(request_data['password']) < 8:
+            valid = False
+            errors.append('Password must be at least 8 characters.')
+        if request_data['password_confirm'] == "":
+            valid = False
+            errors.append('Please confirm password')
+        if request_data['password_confirm'] != request_data['password']:
+            valid = False
+            errors.append('Passwords do not match')
+        return valid, errors
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    alias = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
+
+    def __str__(self):
+        return self.name
--- Django/beltreviewer/apps/beltreview/views.py
+++ Django/beltreviewer/apps/beltreview/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
+from models import User
 # the index function is called when root is visited
 def index(request):
-    response = "Hello, I am your first request!"
+    return render(request, "beltreview/index.html")
+
+def register(request):
+    valid = User.objects.validate_data(request.POST)
+    if valid[0] == True:
+        user = User.objects.create(name=request.POST['name'], alias=request.POST['alias'], email=request.POST['email'], password=request.POST['password'])
+        request.session['userid'] = user.id
+        return redirect ("/books")
+    else: 
+        for message in valid[1]:
+            messages.error(request, message)
+        return redirect ("/")
+
+def books(request):
+    response = ("Welcome user num: {}".format(request.session['userid']))
     return HttpResponse(response)
+
+def login(request):
+    email = request.POST['email']
+    try:
+        user = User.objects.get(email=email)
+    except:
+        messages.error(request, "User does not exist")
+        return redirect ("/")
+    if request.POST['password'] != user.password:
+        messages.error(request, "Wrong password")
+        return redirect ("/")
+    request.session['userid'] = user.id
+    return redirect ("/books")
